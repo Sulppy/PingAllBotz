0f7944be17e5676479ad93eb7112d133abb7ccba
HellCup <nikolajpit@mail.ru>
2023-10-16 00:18:24 +0300

    Нашел путь к решению задачи

--- Costil/getchatu.py
+++ Costil/getchatu.py
@@ -1,10 +1,13 @@
 from pyrogram import Client
 import config
+
 api_id = config.PROG_ID
 api_hash = config.PROG_HASH
 bot_token = config.TG_TOKEN
+
+
 async def get_chat_members(chat_id):
-    app = Client("Имя | Бот", api_id=api_id, api_hash=api_hash, bot_token=bot_token, in_memory=True)
+    app = Client("PyroBot", api_id=api_id, api_hash=api_hash, bot_token=bot_token, in_memory=True)
     chat_members = []
     await app.start()
     async for member in app.get_chat_members(chat_id):
