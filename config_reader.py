ec928dd870b7388852fb18ea3e6f6a811ed53030
HellCup <nikolajpit@mail.ru>
2023-10-27 00:54:22 +0300

    Добавил в .env чтение айди админа для команды /add

--- config_reader.py
+++ config_reader.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     tg_token: SecretStr
     prog_id: SecretStr
     prog_hash: SecretStr
+    admin_id: SecretStr
 
     # Начиная со второй версии pydantic, настройки класса настроек задаются
     # через model_config
