05feea44734c84b30f820a7794928a7d04c12dd4
Sulppy <nikolajpit@mail.ru>
2025-02-11 09:14:48 +0300

    Rework database structure

--- src/initdb.py
+++ src/initdb.py
@@ -6,18 +6,21 @@ bd = config.database_name.get_secret_value()
 def init_db():
     conn = sqlite3.connect(bd)
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS chats("
-                "chat_id INTEGER NOT NULL PRIMARY KEY, "
-                "chat_title TEXT, "
-                "chat_type TEXT NOT NULL)"
-                )
-    conn.commit()
-    # Если её нет, то создаётся таблица юзеров
-    cur.execute("CREATE TABLE IF NOT EXISTS users("
+    cur.execute("CREATE TABLE IF NOT EXISTS chat("
+                "id INTEGER PRIMARY KEY,"
+                "invited_user_id INTEGER NOT NULL);")
+    cur.execute("CREATE TABLE IF NOT EXISTS chat_user("
                 "user_id INTEGER NOT NULL, "
-                "user_name TEXT, "
-                "url_ping TEXT NOT NULL, "
-                "chat_id INTEGER NOT NULL)"
+                "chat_id INTEGER NOT NULL, "
+                "PRIMARY KEY (user_id, chat_id),"
+                "CONSTRAINT fk_chat_id FOREIGN KEY (chat_id) REFERENCES chat(id));"
                 )
     conn.commit()
     conn.close()
+
+def check_chat(chatid: int):
+    conn = sqlite3.connect(bd)
+    cur = conn.cursor()
+    cur.execute(f"SELECT chat_id FROM chat WHERE chat_id = {chatid};")
+    result = cur.fetchone()
+    return result is not None
