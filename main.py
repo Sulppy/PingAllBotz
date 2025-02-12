3726e45343b9ceb5d607a8793c4408439681889d
Sulppy <nikolajpit@mail.ru>
2025-02-12 11:04:41 +0300

    Add input in command line

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 import sys
+import threading
+from threading import Thread
+
 from handlers import database, allping, superuser
 
 from config_reader import config
@@ -12,6 +15,17 @@ from src.initdb import init_db
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.tg_token.get_secret_value())
+
+class BColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 async def main() -> None:
@@ -34,7 +48,17 @@ async def main() -> None:
     # And the run events dispatching
     await bot.delete_webhook(drop_pending_updates=True)
     print("Bot started.")
+    async def thread():
+        while True:
+            x = input()
+            if x in {"exit", "quit", "q"}:
+                await dp.stop_polling()
+                break
+            else:
+                print(f"{BColors.WARNING}Неверная команда.{BColors.ENDC}")
+    asyncio.create_task(thread())
     await dp.start_polling(bot)
+    print("Bot stopped.")
 
 
 if __name__ == "__main__":
